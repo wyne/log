25e600a23720b44bfa6acab20be333426cdcae2e
Matt Vaznaian <matt@stackmob.com>
2013-08-19 00:31:20 -0700

    prod keys

--- site.js
+++ site.js
@@ -12,10 +12,10 @@ function getParameterByName(name){
 StackMob.init({
     appName: "coffeelog",
     clientSubdomain: "bennettbluebottlecoffeecom",
-    //publicKey: "e8f36db3-d231-438c-b6f7-4f7affed07ce",
-    //apiVersion: 1
-    publicKey: "8480bb0b-99bc-4acc-abc9-0a19bd5eab1e",
-    apiVersion: 0
+    publicKey: "e8f36db3-d231-438c-b6f7-4f7affed07ce",
+    apiVersion: 1
+    //publicKey: "8480bb0b-99bc-4acc-abc9-0a19bd5eab1e",
+    //apiVersion: 0
 });
 
 
