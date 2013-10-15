20f23b1734d770789b7f3be3fce13fec9c04e482
bennetthamiltoncross <bennett.cross@gmail.com>
2013-10-15 16:20:53 -0700

    Update site.js

--- site.js
+++ site.js
@@ -13,7 +13,8 @@ StackMob.init({
     appName: "coffeelog",
     clientSubdomain: "bennettbluebottlecoffeecom",
     publicKey: "e8f36db3-d231-438c-b6f7-4f7affed07ce",
-    apiVersion: 1
+    apiVersion: 1,
+    useRelativePathForAjax: false
     //publicKey: "8480bb0b-99bc-4acc-abc9-0a19bd5eab1e",
     //apiVersion: 0
 });
